6ae92b582d9e77ff8987e21606110780bf43bd9f
MRayhanHakim <mrayhan.hakim33@gmail.com>
2026-06-10 12:21:43 +0700

    fix: memperbaiki konsistensi

--- src/config/midtrans.js
+++ src/config/midtrans.js
@@ -3,9 +3,8 @@ const midtransClient = require("midtrans-client");
 // Inisialisasi Snap API Client secara terpusat
 const snap = new midtransClient.Snap({
   isProduction: false, // Ubah ke true jika sudah Production rilis
-  serverKey:
-    process.env.MIDTRANS_SERVER_KEY || "Mid-server-q5haZkC8ubhsRXAaXYaVyDwz",
-  clientKey: process.env.MIDTRANS_CLIENT_KEY || "Mid-client-yeSrRc9OZS_0BHTE",
+  serverKey: process.env.MIDTRANS_SERVER_KEY || "",
+  clientKey: process.env.MIDTRANS_CLIENT_KEY || "",
 });
 
 module.exports = snap;
